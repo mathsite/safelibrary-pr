1272c76daf43fa9aa8aaee4d9413aad914961642
1.9.8.2.6 (AJM) <74386189+ajm19826@users.noreply.github.com>
2025-05-08 19:02:42 -0400

    Update app.py

--- programs/assets/multicodecompiler/app.py
+++ programs/assets/multicodecompiler/app.py
@@ -124,4 +124,4 @@ def cleanup(exception=None):
 
 
 if __name__ == "__main__":
-    app.run(debug=True) # Remove debug=True for production
+        app.run(debug=True) # Remove debug=True for production
